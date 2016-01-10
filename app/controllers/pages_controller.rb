a88082b9494b8176c85bf8f5e54b13775128ddf0
lejandro83 <lejandro83@gmail.com>
2016-01-10 09:46:09 -0600

    Heroku push

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about    
+  end
+
+  def link_to_blank(body, url_options = {}, html_options = {})
+    link_to(body, url_options, html_options.merge(target: "_blank"))
+  end
+end
